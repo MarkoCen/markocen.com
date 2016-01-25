d15e5dad0078edc930648433c200211ea82278b9
Marko Cen <markocen@gmail.com>
2016-01-24 22:21:16 -0500

    implement blogs

--- modules/constants.js
+++ modules/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB_CON: process.env.DB_CON || 'mongodb://localhost:27017/blog',
+    DB_CON: 'mongodb://markocen:pi31415926@ds049925.mongolab.com:49925/blog',
     SERVER_PORT: process.env.SERVER_PORT || 5555
 }
