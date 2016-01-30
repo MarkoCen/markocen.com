d4b47b015f8b2eb9a3044529083b47e967e01e34
Marko Cen <markocen@gmail.com>
2016-01-30 13:57:49 -0500

    update my blog site

--- public/dist/blog.js
+++ public/dist/blog.js
@@ -1,8 +1,22 @@
 ;(function ($) {
+
+
     $('.blog-item').on('click', function (event) {
         if(event.target.className.indexOf('ctag') < 0){
             var articleId = this.getAttribute('data-article-id') || '';
             window.location.assign(window.location.origin + '/blog/' + articleId);
+        }
+    });
+
+    $('.search-submit').on('click', function(event){
+        var queryString = $('#search-input').ele[0].value;
+        window.location.assign(window.location.origin + '/blog?tag=' + queryString);
+    });
+
+    $('#search-input').on('keypress', function(event){
+        if(event.keyCode === 13){
+            var queryString = event.target.value;
+            window.location.assign(window.location.origin + '/blog?tag=' + queryString);
         }
     })
 })(window.$);
