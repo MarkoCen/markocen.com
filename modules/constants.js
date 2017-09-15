5f2529042013c32cc6996d7e8231e5824c3161a8
Marko Cen <markocen@gmail.com>
2017-09-15 16:01:59 -0400

    update port

--- modules/constants.js
+++ modules/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
     //DB_CON: 'mongodb://localhost:27017/blog',
     DB_CON: 'mongodb://markocen:pi31415926@ds049925.mongolab.com:49925/blog',
-    SERVER_PORT: process.env.SERVER_PORT || 5555
+    SERVER_PORT: 5555
 }
