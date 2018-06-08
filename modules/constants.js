6c7c153ff6bd14f9b989f7cebf74f1c348af1180
Marko Cen <markocen@gmail.com>
2018-06-07 22:40:09 -0400

    start to redo my blog :)

--- modules/constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //DB_CON: 'mongodb://localhost:27017/blog',
-    DB_CON: 'mongodb://markocen:pi31415926@ds049925.mongolab.com:49925/blog',
-    SERVER_PORT: 5555
-}
--- modules/db.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-var constants = require('./constants');
-
-module.exports = mongoose.createConnection();
