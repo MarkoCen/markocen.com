d72cfdf72b1d5451661a138ad2847a6ae12ac651
Marko Cen <markocen@gmail.com>
2018-06-10 23:35:29 -0400

    lint

--- src/routers/root.ts
+++ src/routers/root.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 
 const router: Router = Router();
 
 router.get('/greetings', (req: Request, res: Response) => {
-    res.end('hello there')
-})
+    res.end('hello there');
+});
 
 export default router;
