9382fc1b3592130583f0f4c9ba0b62c78ddf5178
Marko Cen <markocen@gmail.com>
2018-06-11 21:35:14 -0400

    split client & server

---
+++ client/pages/Main.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    public render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+export default MainPage;
